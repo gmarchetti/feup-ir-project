96c0c304def72a5be09a008eb7da276e23663214
Guilherme Marcetti <guilherme.marchetti@gmail.com>
2025-04-29 18:07:28 +0100

    fixed tests for cross embedding, renamed hybrid notebook

--- tests/test_cross_ranker.py
+++ tests/test_cross_ranker.py
@@ -1,6 +1,6 @@
 import logging
 import pandas as pd
-
+import os
 from rankers.cross_embedding_ranker import CrossRanker
 
 PATH_COLLECTION_DATA = '/home/guilherme/workspace/feup-ir-project/data/subtask_4b/subtask4b_collection_data.pkl'
@@ -16,11 +16,25 @@ sample_query = df_query[:5]
 logging.basicConfig(level=logging.DEBUG)
 
 def test_can_instantiate():
-    cross_ranker = CrossRanker()
+    dir_list = os.listdir("models/cross-embedding")
+    dir_list.sort()
+
+    latest_checkpoint = dir_list[-1]
+
+    model_name = f"models/cross-embedding/{latest_checkpoint}"
+
+    cross_ranker = CrossRanker(model_name)
     assert cross_ranker != None
 
 def test_get_scores_length():
-    cross_ranker = CrossRanker()
+    dir_list = os.listdir("models/cross-embedding")
+    dir_list.sort()
+
+    latest_checkpoint = dir_list[-1]
+
+    model_name = f"models/cross-embedding/{latest_checkpoint}"
+
+    cross_ranker = CrossRanker(model_name)
     scores = cross_ranker.get_scores(sample_query.loc[0, "tweet_text"], sample_collection)
     
     assert len(scores) == len(sample_collection)
