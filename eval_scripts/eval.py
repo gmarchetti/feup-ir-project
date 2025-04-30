9d4babea808f4ba14fe49627799a3d1bf17c1dce
Guilherme Marcetti <guilherme.marchetti@gmail.com>
2025-04-30 20:08:55 +0100

    extracted eval_scripts to new module, re-ran baseline and bi-encoder to measure avg gold ref in subset

---
+++ eval_scripts/eval.py
@@ -0,0 +1,14 @@
+def get_performance_mrr(data, col_gold, col_pred, list_k = [1, 5, 10]):
+    d_performance = {}
+    for k in list_k:
+        data["in_topx"] = data.apply(lambda x: (1/([i for i in x[col_pred][:k]].index(x[col_gold]) + 1) if x[col_gold] in [i for i in x[col_pred][:k]] else 0), axis=1)
+        d_performance[k] = data["in_topx"].mean()
+    return d_performance
+
+def get_avg_gold_in_pred(data, col_gold, col_pred, list_k = [1, 5, 10]):
+    d_performance = {}
+    for k in list_k:
+        data["in_topx"] = data.apply(lambda x: (1 if x[col_gold] in [i for i in x[col_pred][:k]] else 0), axis=1)
+        d_performance[k] = data["in_topx"].mean()
+    return d_performance
+    
