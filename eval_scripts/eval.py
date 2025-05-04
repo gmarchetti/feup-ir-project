1bf18438042bb1e5281ef8d2a88355d3276959e1
Guilherme Marcetti <guilherme.marchetti@gmail.com>
2025-05-04 11:33:25 +0100

    results from llm re-ranking, create function to output predictions file, bienc-cross run to generate first prediction

--- eval_scripts/eval.py
+++ eval_scripts/eval.py
@@ -11,4 +11,8 @@ def get_avg_gold_in_pred(data, col_gold, col_pred, list_k = [1, 5, 10]):
         data["in_topx"] = data.apply(lambda x: (1 if x[col_gold] in [i for i in x[col_pred][:k]] else 0), axis=1)
         d_performance[k] = data["in_topx"].mean()
     return d_performance
+
+def create_pred_file(query_set, prediction_columns):
+    query_set['preds'] = query_set[prediction_columns].apply(lambda x: x[:5])
+    query_set[['post_id', 'preds']].to_csv('predictions.tsv', index=None, sep='\t')
     
