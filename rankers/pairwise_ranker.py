506d15e5cf3afce71d7a8ccf4915a968ffed2be6
Guilherme Marcetti <guilherme.marchetti@gmail.com>
2025-05-06 19:13:21 +0100

    removed old unused stuff

--- rankers/pairwise_ranker.py
+++ rankers/pairwise_ranker.py
@@ -37,16 +37,6 @@ class PairwiseRanker:
             
         return self.__tokenizer(full_str, padding="max_length", truncation=True, return_tensors='pt')
 
-    def compare(self, query, doc1, doc2):
-        message = self.preprocess_function(query, doc1, doc2).to('cuda')
-
-        with torch.no_grad():
-            logits = self.__model(**message).logits
-            predicted_class_id = logits.argmax().item()
-
-        # print(f"Predicted class id: {predicted_class_id}")
-        return predicted_class_id - 1
-
     def get_higher_rel_prob(self, query, doc1, doc2, cache):
         doc1_uid = doc1["cord_uid"]
         doc2_uid = doc2["cord_uid"]
@@ -96,38 +86,6 @@ class PairwiseRanker:
         }).sort_values(by=["scores"], ascending=False)
 
         return sorted_uid["cord_uids"].tolist()
-
-        
-    def sort_cached_bubble(self, query, docs):
-        collection_as_dict = docs.to_dict('index')
-        doc_index_list = list(collection_as_dict.keys())
-        cached_response = {idx: {} for idx in doc_index_list}
-        idx = 0
-        while idx < len(doc_index_list)-1:
-            doc1_idx = doc_index_list[idx]
-            doc2_idx = doc_index_list[idx+1]
-            # print(f"comparing idx {collection_as_dict[doc1_idx]["cord_uid"]} with {collection_as_dict[doc2_idx]["cord_uid"]}")
-            comparison_result = 0
-            if doc2_idx in cached_response[doc1_idx].keys():
-                # print("Using cached result")
-                comparison_result = cached_response[doc1_idx][doc2_idx]
-            else:
-                # print("Computing comparison")
-                comparison_result = self.compare(query, collection_as_dict[doc1_idx], collection_as_dict[doc2_idx]) 
-                cached_response[doc1_idx][doc2_idx] = comparison_result
-                cached_response[doc2_idx][doc1_idx] = -comparison_result
-            
-            if comparison_result > 0:
-                # print("Switch places")
-                temp = doc_index_list[idx+1]
-                doc_index_list[idx+1] = doc_index_list[idx]
-                doc_index_list[idx] = temp
-                idx = -1
-            
-            idx += 1
-
-        sorted_uids = [collection_as_dict[index]["cord_uid"] for index in doc_index_list]
-        return sorted_uids
 
     def __init__(self, model_name):
         self.__model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2, reference_compile=False).to("cuda")
